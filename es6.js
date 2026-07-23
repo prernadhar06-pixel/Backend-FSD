da939b8393520afe66765cf78f9e422671e2baa0
prernadhar06-pixel <prernadhar06@gmail.com>
2026-07-23 13:11:22 +0530

    asynchronisation

---
+++ es6.js
@@ -0,0 +1,18 @@
+//javascript 
+//SyntaxErrorFunction
+//function fname(){
+//blockof CommandEvent}
+//function calling
+// passing parameter in function
+function sayHello(){
+    console.log("cse 24 fsd");
+    console.log(arguments);
+    return a+b;
+}
+console/log(sayHello(3,2));
+//arrow function
+//()=>{}
+    const sayHello=()=>{
+        console.log("arrowfunction")
+    }
+    sayHello();
